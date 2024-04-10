decab7b04594597ef1f99359bfaa3429e9820f82
HYUNHO\gusgh <gusgh0910@gmail.com>
2024-04-10 23:18:12 +0900

    vue 가이드 study

--- vue_bootstrap/src/App.vue
+++ vue_bootstrap/src/App.vue
@@ -1,21 +1,70 @@
 <template>
-  <button @click="increment">숫자 세기: {{ count }}</button>
+  <div>
+    <button @click="increment">숫자 세기: {{ count }}</button>
+    <button :disabled="isShow">버튼</button>
+    <div :title="formatDate()">{{formatDate()}}</div>
+    <div>{{isShow}}</div>
+    <button @click="changetype">타입변경</button>
+  
+
+  <p>책을 가지고 있다:</p>
+  <span>{{ publishedBooksMessage }}</span>
+  <button @click="removebook">책 제거</button>
+
+  <div>{{now}}</div>
+  <button @click="changenow">시간변경</button>
+  <div :class="classObject">class</div>
+  </div>
 </template>
 
 <script setup>
- import {ref, onMounted } from 'vue'
+ import {ref, onMounted, computed } from 'vue'
 
  // 반응적인 상태의 속성
  const count = ref(0);
+ const isShow = ref(true);
+ const author = ref({books : ['1', '2', '3']})
+ const now  = computed(() => Date.now());
+ 
+ const classObject = ref({
+  'active' : false, 
+  'text-danger' : true,
+ })
 
  // 속성 값을 변경하고 업데이트 할 수 있는 함수.
  function increment() {
   count.value++
  }
 
+ // v-bind 용 함수
+ function formatDate(){
+  return "2024-04-10";
+ }
+ 
+ // number 타입 count변수를 string으로 변경
+ function changetype(){
+  count.value = '20'
+ }
+
+ // 계산된 속성
+ const publishedBooksMessage = computed(()=> {
+  return author.value.books.length > 0 ? 'Yes' : 'No'
+ })
+
+ // 책제거 함수
+ function removebook (){
+  author.value.books.pop();
+ } 
+ 
+ // now 값 변경 함수
+ function changenow(){
+  now.value = Date.now();
+ }
  // 생명 주기 훅
  onMounted(()=>{
   console.log(`숫자 세기의 초기값은 ${ count.value }입니다.`)
+  console.log(isShow)
+  console.log(isShow.value)
  })
 
   // export default {
@@ -52,6 +101,11 @@
   // }
 </script>
 
+<script>
+export default {
+  
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- vue_bootstrap/src/main.js
+++ vue_bootstrap/src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app');
+// createApp(App).mount('#app');
+
+const app = createApp(App);
+app.mount('#app');
+// TodoList 앱의 컴포넌트 트리
+// App (최상위 컴포넌트)
+// ├─ TodoList
+// │  └─ TodoItem
+// │     ├─ TodoDeleteButton
+// │     └─ TodoEditButton
+// └─ TodoFooter
+//    ├─ TodoClearButton
+//    └─ TodoStatistics
